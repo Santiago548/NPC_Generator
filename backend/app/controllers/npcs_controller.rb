2b1e9f38c3dc118933c76e16ee7e5c84590f2b74
Santiago548 <dominicbarajas548@gmail.com>
2021-03-29 12:08:11 -0600

    added index, show, create ,destroy, update, and params for Npcs controller.

--- backend/app/controllers/npcs_controller.rb
+++ backend/app/controllers/npcs_controller.rb
@@ -1,2 +1,39 @@
 class NpcsController < ApplicationController
+    def index
+        npcs = Npc.all
+        render json: NpcSerializer.new(npc)
+    end
+
+    def show
+        npc = Npc.find(params[:id])
+        render json: NpcSerializer.new(npc)
+    end
+
+    def create
+        npc = Npc.new(npc_params)
+
+        if npc.save
+            render json: NpcSerializer.new(npc)
+        else
+            render json: {error: "NPC couldn't be created"}
+        end
+    end
+
+    def destroy
+        npc = Npc.find(params[:id])
+        npc.destroy
+        render json: {message: `#{npc.name} has been deleted!`}
+    end
+
+    def update
+        npc = Npc.find(params[:id])
+        npc.update(npc_params)
+        render json: NpcSerializer.new(npc)
+    end
+
+    private
+
+    def npc_params
+        params.require(:npc).permit(:name, :race, :sex, :alignment, :description, :strength, :dexterity, :constitution, :wisdome, :charisma, :id, :encounter_id)
+
 end
