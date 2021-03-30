0d391370435c5c5a8a557837fb1fa905c2685a01
Santiago548 <dominicbarajas548@gmail.com>
2021-03-30 15:15:54 -0600

    added a render functionality to npc js file

---
+++ frontend/index.js
@@ -0,0 +1,1 @@
+const npcAdapter = new NpcAdapter
--- frontend/src/npc.js
+++ frontend/src/npc.js
@@ -37,5 +37,23 @@ class Npc{
         this.addEventListeners()
     }
 
+    render(){
+        this.element.innerHTML = `
+        <li>
+        $<span class="npc-name>${this.name}</span>
+        $<span class="npc-race>${this.race}</span>
+        $<span class="npc-sex>${this.sex}</span>
+        $<span class="npc-alignment>${this.alignment}</span>
+        $<span class="npc-strength>${this.strength}</span>
+        $<span class="npc-dexterity>${this.dexterity}</span>
+        $<span class="npc-constitution>${this.constitution}</span>
+        $<span class="npc-wisdom>${this.wisdom}</span>
+        $<span class="npc-charisma>${this.charisma}</span>
+        </li>
+        <button class="delete" data-id="${this.id}">Delete</button>
+        <button class="update" data-id="${this.id}">Update</button>
+        `
+        return this.element
+    }
 
 }
