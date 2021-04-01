a2b5c33a1d19fa7a10e5ef2aa6f24190f81baa52
Santiago548 <dominicbarajas548@gmail.com>
2021-04-01 16:59:36 -0600

    commented out capbility to delete encounter subit and encounter form

--- frontend/src/encountersAdapter.js
+++ frontend/src/encountersAdapter.js
@@ -19,53 +19,49 @@ class EncountersAdapter{
         }
     }
 
-    handleFormSubmit = (e) => {
-        e.preventDefault()
-        const name = document.getElementById('encounter-name').value
-        const description = document.getElementById('encounter-description').value
-        const environment = document.getElementById('encounter-environment').value
+    // handleFormSubmit = (e) => {
+    //     e.preventDefault()
+    //     const name = document.getElementById('encounter-name').value
         
-        let newEncounter = {
-            name,
-            description,
-            environment
-        }
+    //     let newEncounter = {
+    //         name
+    //     }
 
-        let configEncounter =  {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            },
-            body: JSON.stringify(newEncounter)
-        }
+    //     let configEncounter =  {
+    //         method: 'POST',
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json"
+    //         },
+    //         body: JSON.stringify(newEncounter)
+    //     }
 
-        fetch(this.baseUrl, configEncounter)
-        .then(res => res.json())
-        .then(json => {
-            let encounter = new Encounter(json.data.attributes)
-            encounter.attachToDom()
-        })
-    }
+    //     fetch(this.baseUrl, configEncounter)
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         let encounter = new Encounter(json.data.attributes)
+    //         encounter.attachToDom()
+    //     })
+    // }
 
-    deleteEncounter(id){
-        let configEncounter = {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            }
-        }
+    // deleteEncounter(id){
+    //     let configEncounter = {
+    //         method: "DELETE",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json"
+    //         }
+    //     }
 
-        fetch(this.baseUrl + `/${id}`, configEncounter)
-        .then(res => res.json())
-        .then(json => {
-            alert(json.message)
-        })
-        Encounter.all = Encounter.all.filter(i => i.id !=id)
+    //     fetch(this.baseUrl + `/${id}`, configEncounter)
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         alert(json.message)
+    //     })
+    //     Encounter.all = Encounter.all.filter(i => i.id !=id)
         
-        let encounter = document.getElementById(`encounter-${id}`)
-        encounter.remove()
-    }
+    //     let encounter = document.getElementById(`encounter-${id}`)
+    //     encounter.remove()
+    // }
 
 }
