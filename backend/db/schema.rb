15ff41170e01825a6e9c6142bd08a9dcaee5f7e2
Santiago548 <dominicbarajas548@gmail.com>
2021-04-01 16:39:13 -0600

    added Encounter Serializer back in and modyfiying npcs migration to have an encounter id.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,24 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_221405) do
-
-  create_table "npcs", force: :cascade do |t|
-    t.string "name"
-    t.string "race"
-    t.string "sex"
-    t.string "alignment"
-    t.string "melee"
-    t.string "ranged"
-    t.integer "health"
-    t.string "armor"
-    t.integer "strength"
-    t.integer "dexterity"
-    t.integer "constitution"
-    t.integer "wisdom"
-    t.integer "charisma"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
